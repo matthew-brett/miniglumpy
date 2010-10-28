676c5e19f9ea09d7839922928222c479af67a1cf
Matthew Brett <matthew.brett@gmail.com>
2010-10-28 01:49:11 +0100

    RF - removed duplicate Glice object

--- miniglumpy/textures.py
+++ miniglumpy/textures.py
@@ -3,16 +3,7 @@
     A texture is an image loaded into video memory that can be efficiently
     drawn to the framebuffer.
 '''
-# Modified from texture.py in glumpy
-#-----------------------------------------------------------------------------
-# Glumpy copyright (C) 2009-2010  Nicolas P. Rougier
-#
-# Distributed under the terms of the BSD License. The full license is in
-# the file COPYING, distributed as part of this software.
-#-----------------------------------------------------------------------------
-#
-# Modifications by Matthew Brett also under terms of BSD license - see
-# COPYING.txt
+# Modified from texture.py in glumpy - see COPYING.txt
 
 import numpy as np
 
@@ -145,41 +136,3 @@ class Texture2D(Texture1D):
         gl.glTexCoord2f(s[1], 0), gl.glVertex2f(x+w, y+h)
         gl.glTexCoord2f(s[1], 1), gl.glVertex2f(x+w, y)
         gl.glEnd()
-
-
-class Glice(object):
-    def __init__(self, arr, shader=None, cmap=None, vmin=None, vmax=None):
-        self._texture = Texture2D(arr)
-        self._arr = arr
-        if shader is None:
-            shader = gshaders.Nearest(True, False)
-        self.shader = shader
-        if cmap is None:
-            cmap = np.tile(
-                np.linspace(0, 1, 512)[:,None], (1,3)).astype(np.float32)
-        self.cmap = cmap
-        self._lut = Texture1D(cmap)
-        if vmin is None:
-            vmin = arr.min()
-        if vmax is None:
-            vmax = arr.max()
-        self.vmin, self.vmax = vmin, vmax
-
-    def set_data(self, arr):
-        self._texture.set_data(arr)
-        self._arr = arr
-        self.update()
-
-    def update(self):
-        s = self._lut.width
-        vmin = self.vmin
-        vmax = self.vmax
-        self._texture.update(bias = 1.0/(s-1)-vmin*((s-3.1)/(s-1))/(vmax-vmin),
-                             scale = ((s-3.1)/(s-1))/(vmax-vmin))
-
-    def blit(self, x, y, w, h):
-        ''' Blit array onto active framebuffer. '''
-        self.shader.bind(self._texture, self._lut)
-        self._texture.blit(x,y,w,h)
-        self.shader.unbind()
-
